60f9325582eb31f84686ab64acc410f443a4f754
alejo <alejo.80.123@gmail.com>
2022-04-07 18:25:21 -0500

    70%

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = () => {
             <footer className="footer">
                 <a href="https://www.linkedin.com/in/sergio-alejandro-hernandez-zambrano-090070212/"
                    target="_blank" className="pr-5 flex justify-center items-center">
-                    <FaLinkedin size={23} className="pr-1"/> Linkedin
+                    <FaLinkedin size={18} className="pr-1"/> Linkedin
                 </a>
                 <a href="https://github.com/Sergio-mix" target="_blank"
                    className="pr-5 flex justify-center items-center">
-                    <FaGithub size={23} className="pr-1"/> Github
+                    <FaGithub size={18} className="pr-1"/> Github
                 </a>
-                <a href="mailto:alejo.80.123@gmail.com" target="_blank" className="pr-5 flex justify-center items-center">
-                    <BsMailbox size={23} className="pr-1"/>Gmail
+                <a href="mailto:alejo.80.123@gmail.com" target="_blank" className="pr-3 flex justify-center items-center">
+                    <BsMailbox size={18} className="pr-1"/>Gmail
                 </a>
-                <a href="https://wa.me/573222406726" target="_blank" className="pr-5 flex justify-center items-center">
-                    <BsWhatsapp size={23} className="pr-1"/>Whatsapp
+                <a href="https://wa.me/573222406726" target="_blank" className="pr-3 flex justify-center items-center">
+                    <BsWhatsapp size={18} className="pr-1"/>Whatsapp
                 </a>
             </footer>
         </div>
