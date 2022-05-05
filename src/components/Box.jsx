3be18344e46042a6090d2ef54556913ef65c7cc8
alejo <alejo.80.123@gmail.com>
2022-05-05 16:51:17 -0500

    100%

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {hover} from "@testing-library/user-event/dist/hover";
 
 const Box = (props) => {
 
--- src/components/ModalWork.jsx
+++ src/components/ModalWork.jsx
@@ -1,6 +1,5 @@
-import {useState} from "react";
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel'
+
 
 const ModalWork = (props) => {
 
@@ -13,47 +12,9 @@ const ModalWork = (props) => {
 
     return (
         <div className={"modal-container " + isOpen(props.show)}>
-            <div className="modal">
-                <h1 className={"text-4xl text-white font-bold"}>{props.data.title}</h1>
-                <div className={"carousel"}>
-                    <Carousel >
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-                                alt="First slide"
-                            />
-                            <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-                                alt="Second slide"
-                            />
+            <div className="modal-div">
+                <h1 className={"text-4xl text-pink-600"}>{props.data.title}</h1>
 
-                            <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
-                                alt="Third slide"
-                            />
-
-                            <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel>
-                </div>
                 <button onClick={props.close}>Cerrar</button>
             </div>
         </div>
