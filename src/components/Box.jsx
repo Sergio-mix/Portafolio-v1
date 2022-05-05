797cfc389114cb7100e929cfa50ff039d2e7233d
alejo <alejo.80.123@gmail.com>
2022-05-05 16:14:19 -0500

    100%

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 const Box = (props) => {
 
     return (
-        <div>
-            <div
-                className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div hover:scale-110 duration-300">
-                <div className='opacity-0 group-hover:opacity-100 content-div-items'>
-                    <h1 className='text-4xl font-bold font-size-inherit tracking-wider'>
-                        {props.data.title}
-                    </h1>
-                    <button
-                        className='text-center rounded-lg px-4 py-3 m-2 bg-pink-600 text-white font-bold text-lg hover:scale-110 duration-300 mt-4'
+        <div
+            style={{backgroundImage: `url(${props.data.imageHome})`}}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div hover:scale-110 duration-500'>
+            <div className='opacity-0 group-hover:opacity-100'>
+              <h1 className='text-3xl font-bold text-white tracking-wider'>
+               {props.data.title}
+              </h1>
+                <div className='pt-8 text-center'>
+                        <button
+                            className='text-center rounded-lg px-4 py-3 m-2 bg-pink-600 text-white font-bold text-lg hover:scale-110 duration-500'
                         onClick={props.event}>
-                        Open
-                    </button>
+                            Open
+                        </button>
                 </div>
-                <img className={"img"} src={props.data.imageHome} alt={""}/>
             </div>
         </div>
     );
--- src/components/ModalWork.jsx
+++ src/components/ModalWork.jsx
@@ -1,22 +1,60 @@
 import {useState} from "react";
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel'
 
 const ModalWork = (props) => {
 
     function isOpen(isOpen) {
         if (isOpen)
-            return ("display-block");
+            return ("show");
         else
-            return ("display-none");
+            return (" ");
     }
 
     return (
-        <div className={isOpen(props.show)}>
-            <div className="modal-container">
-                <div className="modal">
-                    <h1 className={"text-4xl text-white font-bold"}>{props.data.title}</h1>
+        <div className={"modal-container " + isOpen(props.show)}>
+            <div className="modal">
+                <h1 className={"text-4xl text-white font-bold"}>{props.data.title}</h1>
+                <div className={"carousel"}>
+                    <Carousel >
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                                alt="Second slide"
+                            />
 
-                    <button onClick={props.close}>Cerrar</button>
+                            <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                                alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
+                <button onClick={props.close}>Cerrar</button>
             </div>
         </div>
     );
